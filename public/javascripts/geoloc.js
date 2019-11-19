5c52e3009d1ed493e9257ceeb8a4fe5f942cd64e
Keighley McFarland <mcfarland.ke@gmail.com>
2019-11-19 09:55:40 +0100

    remove console logs

--- public/javascripts/geoloc.js
+++ public/javascripts/geoloc.js
@@ -2,11 +2,9 @@ const getLocation = () => {
   navigator.geolocation.getCurrentPosition(function(position) {
     let lat = position.coords.latitude;
     let lng = position.coords.longitude;
-    console.log("lat lng", lat, lng);
     localStorage.setItem("lat", lat);
     localStorage.setItem("lng", lng);
-    console.log("localStor lat", localStorage.getItem("lat"));
-    console.log("localStor lng", localStorage.getItem("lng"));
+
     if (document.getElementById("lat")) {
       document.getElementById("lat").value = localStorage.getItem("lat");
     }
